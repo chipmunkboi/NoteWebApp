c9b084cfa8a95ef2c475df8c647ce2e5d651b3f7
Jeffrey Zhang <jeffreyzhang2@gmail.com>
2020-05-28 01:20:13 -0700

    finished!
    Finished porting star ratings over but uses values of 0 1 and 2

--- static/components/thumbrater/thumbrater.js
+++ static/components/thumbrater/thumbrater.js
@@ -8,19 +8,18 @@
 
     thumbrater.data = function() {
         var data = {
-            num_display: 0,
-            num_assigned: 0,
-            thumb_state: [1, 2, 3],
+            num_display: 0, 
+            num_assigned: 0, 
+            thumb_state: [1,2], //1 is up button and 2 is down button
             get_url: this.url,
             set_url: this.callback_url,
+            original_state: null,
         };
         thumbrater.methods.load.call(data);
         return data;
     };
 
     thumbrater.methods.thumbs_over = function (thumb_idx) {
-        // When hovering over a star, we display that state.
-        console.log("Over:", thumb_idx);
         let self = this;
         self.num_display = thumb_idx;
     };
@@ -35,6 +34,14 @@
         // Sets and sends to the server the number of stars.
         let self = this;
         self.num_assigned = thumb_idx;
+        console.log("Num Assigned:", self.num_assigned);
+        console.log("Original State", self.original_state);
+        console.log("Thumb IDX", thumb_idx);
+        console.log("");
+        if(self.original_state == thumb_idx){
+            self.num_assigned = 0;
+        }
+        self.original_state = self.num_assigned;
         axios.get(self.set_url,
             {params: {rating: self.num_assigned}});
     };
@@ -47,6 +54,7 @@
             .then(function(res) {
                 self.num_assigned = res.data.rating;
                 self.num_display = res.data.rating;
+                self.original_state = res.data.rating;
             })
     };
 
